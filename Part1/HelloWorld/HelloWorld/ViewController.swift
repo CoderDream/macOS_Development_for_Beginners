271e9dc53a603fbf7e17f6ac38701ab1897cb77c
CoderDream <coderdream@gmail.com>
2021-01-15 11:58:00 +0800

    Part 1 complete 20210115_1157

---
+++ Part1/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by CoderDream on 2021/1/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var nameField: NSTextField!
+    
+    @IBOutlet weak var helloLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    @IBAction func sayButtonClicked(_ sender: Any) {
+        var name = nameField.stringValue
+        if name.isEmpty {
+          name = "World"
+        }
+        let greeting = "Hello \(name)!"
+        helloLabel.stringValue = greeting
+    }
+    
+}
+
